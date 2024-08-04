27d06ec553dbb7b028f35bb6731463d12ba70642
하이 <kkm07133@naver.com>
2024-08-04 23:11:58 +0900

    bugfix..

--- cmd_backend/src/main/java/com/gdsc/cmd/global/security/jwt/JwtProperties.java
+++ cmd_backend/src/main/java/com/gdsc/cmd/global/security/jwt/JwtProperties.java
@@ -11,7 +11,7 @@ import java.util.Base64;
 @Configuration
 public class JwtProperties { // 환경 변수 값을 필드로 구현
 
-    @Value("${JWT_SECRET_KEY}")
+    @Value("${jwt.secret-key}")
     private String secretKey;
 
     @Value("${jwt.accessExp}")
