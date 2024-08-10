015bf39541fd690cfc1c02e9fa69f1dfc92161ed
하이 <kkm07133@naver.com>
2024-08-10 22:42:47 +0900

    usersearch

---
+++ cmd_backend/src/main/java/com/gdsc/cmd/domain/user/controller/dto/UserSearchResponse.java
@@ -0,0 +1,16 @@
+package com.gdsc.cmd.domain.user.controller.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class UserSearchResponse {
+    private String accountId;
+    private int classNumber;
+
+    @Builder
+    public UserSearchResponse(String accountId, int classNumber) {
+        this.accountId = accountId;
+        this.classNumber = classNumber;
+    }
+}
---
+++ cmd_backend/src/main/java/com/gdsc/cmd/domain/user/service/AllUserSearchService.java
@@ -0,0 +1,26 @@
+package com.gdsc.cmd.domain.user.service;
+
+import com.gdsc.cmd.domain.user.controller.dto.UserSearchResponse;
+import com.gdsc.cmd.domain.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AllUserSearchService {
+    private final UserRepository userRepository;
+
+    public List<UserSearchResponse> findAll(){
+        return userRepository.findAll().stream().
+                map(user -> UserSearchResponse.builder()
+                        .accountId(user.getAccountId())
+                        .classNumber(user.getClassNumber())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+
+}
