1abf1932e7aa51b6710abaaa7f05d23a62d43957
RyuJeaMin <kariel7673@gmail.com>
2024-08-27 19:06:07 +0900

    email 로 password 찾기

---
+++ cmd_backend/src/main/java/com/gdsc/cmd/global/findpassword/UserCheckByEmailService.java
@@ -0,0 +1,8 @@
+package com.gdsc.cmd.global.findpassword;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserCheckByEmailService {
+    public boolean checkEmail(String email) {}
+}
